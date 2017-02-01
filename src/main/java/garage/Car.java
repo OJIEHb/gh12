d291bc7b322c52493cd489170d76b7183339ab54
OJIEHb <geiko2015@gmail.com>
2017-02-01 19:25:10 +0200

    Add annotation-based container configuration

--- src/main/java/garage/Car.java
+++ src/main/java/garage/Car.java
@@ -2,14 +2,18 @@ package garage;
 
 import garage.carParts.Engine;
 import garage.carParts.Wheel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by andrey on 29.01.17.
  */
+@Component
 public class Car {
     private Wheel wheel;
     private Engine engine;
 
+    @Autowired
     public Car(Wheel wheel, Engine engine) {
         this.wheel = wheel;
         this.engine = engine;
--- src/main/java/garage/carParts/Wheel.java
+++ src/main/java/garage/carParts/Wheel.java
@@ -1,11 +1,16 @@
 package garage.carParts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by andrey on 29.01.17.
  */
+@Component
 public class Wheel {
     private Tyres tyres;
 
+    @Autowired
     public Wheel(Tyres tyres) {
         this.tyres = tyres;
     }
